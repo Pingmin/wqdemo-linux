5a8968ed4e20cd22ab42d1e7c592817efd32bbbf
Pingmin Fenlly Liu <Pingmin@Fenlly.org>
2013-08-26 23:05:13 +0800

    replace _OpenFile() by _OpenStream()
    decoding is ok
    20130821

---
+++ Makefile
@@ -0,0 +1,76 @@
+
+VERSION = 1.0
+
+CC = g++
+
+OPTIONS = -Wall
+#OPTIONS += -O3
+#OPTIONS += -g
+
+# symbol hidden option (for gcc v4.0+)
+SOPTIONS = -fvisibility=hidden -fvisibility-inlines-hidden
+
+# SOURCE CODE
+CC_SRC = $(wildcard  *.cpp)
+C_SRC = $(wildcard  *.c)
+
+# OBJECTS
+CC_OBJS = $(patsubst %.cpp,./build/%.o,$(CC_SRC))
+C_OBJS = $(patsubst %.c,./build/%.o,$(C_SRC))
+OBJS = $(CC_OBJS) $(C_OBJS)
+
+# DEPS
+DEPS = $(patsubst %.o,%.d,$(OBJS))
+
+# INCLUDE DIR
+INC_DIR = -I. -I./include
+
+
+# LIB_DIR
+#LIB_DIR = -L. -L./lib  -L/lib -L/usr/lib -L/usr/local/lib
+LIB_DIR = -L. -L./lib
+
+# LIBS
+#LIBS = -lwqplaysdk -lsvacdec -lh264dec.so -lintlc
+LIBS = -lwqplaysdk
+
+LDFLAGS = $(LIB_DIR) $(LIBS)
+
+CPPFLAGS = $(SOPTIONS) $(INC_DIR) $(DFLAGS)
+
+EXE = wqdemo
+
+# depends on wqdemo.cpp
+EXE_OBJ = wqdemo.o
+
+
+all: $(EXE)
+
+include $(DEPS)
+
+$(EXE):$(OBJS)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+./build/%.o:%.cpp
+	$(CC) $(OPTIONS) -o $@ -c $< $(CPPFLAGS)
+
+./build/%.o:%.c
+	$(CC) $(OPTIONS) -o $@ -c $< $(CPPFLAGS)
+
+./build/%.d:%.cpp
+	@set -e; rm -f $@; \
+	$(CC) -MM $(CPPFLAGS) $< > $@.$$$$; \
+	sed 's,.*\.o[ :]*,$(patsubst %.d,%.o,$@) $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+./build/%.d:%.c
+	@set -e; rm -f $@; \
+	$(CC) -MM $(CPPFLAGS) $< > $@.$$$$; \
+	sed 's,.*\.o[ :]*,$(patsubst %.d,%.o,$@) $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+
+clean:
+	rm -f $(EXE)
+	rm -f $(DEPS)
+	rm -f $(OBJS)
--- wqdemo.cpp
+++ wqdemo.cpp
@@ -1,3 +1,17 @@
+/**
+ * wqplaysdk demo
+ *
+ * Copyright (c) Vimicro and ZX Elec
+ *
+ * History:
+ *   20130821 - new created, by Zhou Haitao
+ *   20130821 - replace _OpenFile() by _OpenStream(), by Liu Zeping
+ *
+ * More:
+ *   http://yunpan.cn/QXHiyqZk8G78W
+ *
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,6 +22,8 @@ using namespace WQ;
 
 #define NPORT 0
 
+const int MAX_BUF_SIZE  = 10*1024;
+
 void CALLBACK DecCBFun(long nPort,
 char * pBuf,
 long nSize,
@@ -16,7 +32,7 @@ long nReserved1,
 long nReserved2)
 {
   printf("DecCBFun\n");
-  FILE* pf = fopen("1.yuv","ab+");
+  FILE* pf = fopen("test.yuv","ab+");
   fwrite(pBuf,nSize,1,pf);
   fclose(pf);
 }
@@ -27,6 +43,15 @@ void CALLBACK NeedDecodePasswordFun(long nPort, int nErrorCode, void* pUser)
 
 int main()
 {
+  unsigned int size = 0;
+  unsigned char buf[MAX_BUF_SIZE] = {0,};
+  FILE *fp = fopen("test.vdg", "rb");
+  if (NULL == fp)
+  {
+    printf("fopen failed\n");
+    return -1;
+  }
+
   if(VN_PLAY_SetDecCallBack(NPORT, DecCBFun) == 0)
   {
     printf("VN_PLAY_SetDecCallBack failed\n");
@@ -37,24 +62,51 @@ int main()
     printf("VN_PLAY_SetNeedDecodePasswordCallbackFun failed\n");
     return -1;
   }
-  if(VN_PLAY_OpenFile(NPORT,"20130802_roi.vdg")==0)
+
+/***
+  if(VN_PLAY_OpenFile(NPORT,"test.vdg")==0)
   {
     printf("VN_PLAY_OpenFile failed\n");
     return -1;
   }
-  if (VN_PLAY_Play(NPORT, 0) == 0)
+***/
+  if(VN_PLAY_OpenStream(NPORT, NULL, 0, 1024*1024) == 0)
   {
-    printf("VN_PLAY_Play failed\n");
+    printf("VN_PLAY_OpenStream failed\n");
     return -1;
   }
 
-  while(1)
+  if (VN_PLAY_Play(NPORT, 0) == 0)
   {
+     printf("VN_PLAY_Play failed\n");
+     return -1;
+  }
+
+  while (!feof(fp))
+  {
+/****
     float pos = VN_PLAY_GetPlayPos(NPORT);
     printf("pos = %f\n",pos);
     if(pos>0.95)
     {
-    break;
+      break;
+    }
+*****/
+
+    memset(buf, 0, sizeof(buf));
+		size = fread(buf, sizeof(unsigned char), sizeof(buf), fp);
+
+    if (0 == size)
+    {
+      printf("fread failed\n");
+      continue;
+      //return -1;
+    }
+
+    if (VN_PLAY_InputData(NPORT, buf, size) == 0)
+    {
+      printf("VN_PLAY_InputData failed\n");
+      //return -1;
     }
 
     usleep(100*1000);
@@ -62,19 +114,22 @@ int main()
 
   if(VN_PLAY_Stop(NPORT) == 0)
   {
-   printf("VN_PLAY_Stop failed\n");
-   return -1;
+    printf("VN_PLAY_Stop failed\n");
+    return -1;
   }
   if(VN_PLAY_SetDecCallBack(NPORT, 0) == 0)
   {
-   printf("VN_PLAY_SetDecCallBack failed\n");
-   return -1;
+    printf("VN_PLAY_SetDecCallBack failed\n");
+    return -1;
   }
   if(VN_PLAY_CloseStream(NPORT) == 0)
   {
-   printf("VN_PLAY_CloseStream failed\n");
-   return -1;
+    printf("VN_PLAY_CloseStream failed\n");
+    return -1;
   }
+
+  fclose(fp);
+  fp = NULL;
 
   return 0;
 }
